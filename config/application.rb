e2f63e6cf8c337c57c1119aa453f82e72e670cca
= <=>
2022-07-04 12:12:14 +0300

    +

--- config/application.rb
+++ config/application.rb
@@ -12,16 +12,6 @@ module BeFocused
     config.load_defaults 7.0
     config.serve_static_assets = true
 
-    # Disable Rails's static asset server (Apache or nginx will already do this)
-    # Compress JavaScripts and CSS
-    config.assets.compress = true
-
-    # Don't fallback to assets pipeline if a precompiled asset is missed
-    config.assets.compile = true
-
-    # Generate digests for assets URLs
-    config.assets.digest = true
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
